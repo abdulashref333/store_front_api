be032ad320790c5c9e5d22cf09a18f83b580b108
Abdulrahman <abdul.ashref333@gmail.com>
2022-02-14 16:06:04 +0200

    add port env

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import app from './app';
 import { checkingEnvVariables } from './config/checking-env-variables';
 import Logger from './src/middlewares/logger';
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   checkingEnvVariables();
   // await startDbConnection();
 
-  app.listen(3000, () => Logger.info('Listening on port 3000!'));
+  app.listen(port, () => Logger.info('Listening on port 3000!'));
 };
 
 start();
