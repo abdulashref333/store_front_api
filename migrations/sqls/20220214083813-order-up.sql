ed27c8314d75c215ed8d62725dc77a105b150fbf
Abdulrahman <abdul.ashref333@gmail.com>
2022-02-15 16:32:27 +0200

    update order schema

--- migrations/sqls/20220214083813-order-up.sql
+++ migrations/sqls/20220214083813-order-up.sql
@@ -1,8 +1,7 @@
 CREATE TABLE IF NOT EXISTS orders(
-    order_id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     customer_id BIGINT NOT NULL REFERENCES users(id),
     total DECIMAL(12,2) NOT NULL,
-    billing_address_id VARCHAR(11) NOT NULL,
     order_status VARCHAR(100),
     payment_type VARCHAR(40) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
