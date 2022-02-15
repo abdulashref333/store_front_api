4a8b57233b9c9c4f279a7c1c34154e27a40e03f1
Abdulrahman <abdul.ashref333@gmail.com>
2022-02-15 18:19:11 +0200

    update stratgy of storing passwords

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,14 +1,16 @@
 import bcrypt from 'bcryptjs';
-
+const extraPassStr = process.env.EXTRA_PASSWORD || 'Any_Random_Password_22';
 export class Password {
   static async toHash(password: string): Promise<string> {
     const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const newPassword = extraPassStr + password + extraPassStr;
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
 
     return hashedPassword;
   }
 
   static async compare(storedPassword: string, suppliedPassword: string): Promise<boolean> {
-    return await bcrypt.compare(suppliedPassword, storedPassword);
+    const password = extraPassStr + suppliedPassword + extraPassStr;
+    return await bcrypt.compare(password, storedPassword);
   }
 }
