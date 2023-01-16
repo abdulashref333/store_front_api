be6ff87cc532a1940e20d0717e0d78457fdf6148
Abdelrahman Ashraf <abdul.ashref333@gmail.com>
2023-01-16 10:41:57 +0200

    up: create component script

--- component.sh
+++ component.sh
@@ -1,3 +1,4 @@
+# this is a script to create a new component in the src/components folder
 read componentName
 
 mkdir ./src/$componentName && cd ./src/components/$componentName && touch $componentName.controller.ts $componentName.routes.ts $componentName.model.ts $componentName.schema.ts $componentName.interfaces.ts $componentName.spec.ts
